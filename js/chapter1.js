e424580c991a986a0e3f7c4744e822e8b8045a8c
fm456291-blip <fm456291@gmail.com>
2026-07-29 20:34:33 +0500

    update

---
+++ js/chapter1.js
@@ -0,0 +1,29 @@
+const stars = document.getElementById("stars");
+
+for (let i = 0; i < 250; i++) {
+
+    const star = document.createElement("div");
+
+    star.className = "star";
+
+    star.style.left = Math.random() * 100 + "%";
+
+    star.style.top = Math.random() * 100 + "%";
+
+    const size = Math.random() * 3 + 1;
+
+    star.style.width = size + "px";
+
+    star.style.height = size + "px";
+
+    star.style.animationDelay = Math.random() * 5 + "s";
+
+    stars.appendChild(star);
+
+}
+
+document.getElementById("nextBtn").addEventListener("click", () => {
+
+    alert("Chapter 2 Coming Soon ❤️");
+
+});
